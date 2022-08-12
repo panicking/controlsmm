f46805ea2b06aaa3e45c612a1a0511fc05536842
Michael Trimarchi <michael@amarulasolutions.com>
2022-08-12 15:15:08 +0200

    datelabel: Add propery to set am/pm mode
    Add a propery in order to set am/pm mode in date time label
    
    Signed-off-by: Michael Trimarchi <michael@amarulasolutions.com>

--- datelabel.cpp
+++ datelabel.cpp
@@ -1,31 +1,33 @@
-#include <gtkmm.h>
 #include <gtkmm/entry.h>
-#include <glibmm/datetime.h>
 #include "datelabel.h"
 
 namespace components {
 
-    dateLabel::dateLabel() : Gtk::Label(),
-		    Glib::ObjectBase("dateLabel")
+    dateLabel::dateLabel() : Gtk::Label(), Glib::ObjectBase("dateLabel"),
+                prop_use_ampm(*this, "use-ampm", false)
     {
-        date_slot = sigc::mem_fun(*this, &dateLabel::on_timeout_update_time);
-        on_timeout_update_time();
     }
 
     dateLabel::dateLabel(BaseObjectType* cobject,
-		    const Glib::RefPtr<Gtk::Builder>& builder) : Gtk::Label(cobject),
-		    Glib::ObjectBase("dateLabel")
+                const Glib::RefPtr<Gtk::Builder>& builder) : Gtk::Label(cobject),
+                Glib::ObjectBase("dateLabel"),
+                prop_use_ampm(*this, "use-ampm", false)
     {
         date_slot = sigc::mem_fun(*this, &dateLabel::on_timeout_update_time);
+        use_ampm().signal_changed().connect(sigc::mem_fun(*this, &dateLabel::on_timeout_update_time));
         on_timeout_update_time();
     }
 
-    void dateLabel::on_timeout_update_time(void)
+    void dateLabel::on_timeout_update_time()
     {
         Glib::ustring str;
         Glib::DateTime now = Glib::DateTime::create_now_local();
 
-        str = now.format("%I:%M %p");
+        if (prop_use_ampm)
+            str = now.format("%I:%M %p");
+        else
+            str = now.format("%k:%M");
+
         set_text(str);
 
         Glib::signal_timeout().connect_seconds_once(date_slot, 60 - now.get_second());
--- datelabel.h
+++ datelabel.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <glib.h>
+#include <gtkmm.h>
+#include <glibmm/datetime.h>
 #include <gtkmm/label.h>
 #include <sigc++/sigc++.h>
 
@@ -11,10 +13,13 @@ namespace components {
     dateLabel(BaseObjectType* cobject, const Glib::RefPtr<Gtk::Builder>& refGlade);
     virtual ~dateLabel();
 
+    Glib::PropertyProxy<bool> use_ampm() { return prop_use_ampm.get_proxy(); };
+
     protected:
-    void on_timeout_update_time(void);
+    void on_timeout_update_time();
 
     private:
     sigc::slot<void(void)> date_slot;
+    Glib::Property<bool> prop_use_ampm;
   };
 }
